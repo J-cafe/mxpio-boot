5edde8f421aacc56c4cd2dc8148a0acadd3820f3
MxpIO <i@mxpio.com>
2021-05-08 18:02:22 +0800

    fix

---
+++ mxpio-boot-base-ddm/src/main/java/com/mxpio/mxpioboot/ddm/DdmConfiguration.java
@@ -0,0 +1,16 @@
+package com.mxpio.mxpioboot.ddm;
+
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 模块配置类
+ */
+@Configuration
+@AutoConfigurationPackage
+@ComponentScan
+public class DdmConfiguration {
+
+
+}
