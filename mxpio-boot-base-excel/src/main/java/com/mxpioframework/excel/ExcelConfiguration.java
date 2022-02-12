d907c5cf2b8872d2c5b1e76490e54c2cfeda06eb
mxpio <i@mxpio.com>
2022-02-12 15:12:10 +0800

    新增模块

---
+++ mxpio-boot-base-excel/src/main/java/com/mxpioframework/excel/ExcelConfiguration.java
@@ -0,0 +1,12 @@
+package com.mxpioframework.excel;
+
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@AutoConfigurationPackage
+@ComponentScan
+public class ExcelConfiguration {
+
+}
