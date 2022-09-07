b05527bbcac11b38ef3b5547fabaa1e756db0510
admin <admin@dongliang>
2022-09-07 11:18:32 +0800

    提高认证灵活性，有其他认证方式，可继承SecurityConfiguration即可

--- mxpio-boot-base-security/src/main/java/com/mxpioframework/security/SecurityConfiguration.java
+++ mxpio-boot-base-security/src/main/java/com/mxpioframework/security/SecurityConfiguration.java
@@ -1,11 +1,16 @@
 package com.mxpioframework.security;
 
 import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.mxpioframework.security.anthentication.JwtAuthenticationProvider;
 
 @Configuration
 @AutoConfigurationPackage
@@ -15,5 +20,11 @@ public class SecurityConfiguration {
 	@Bean
 	public PasswordEncoder standardPasswordEncoder() {
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+	
+	@Bean
+	@ConditionalOnMissingBean(AuthenticationProvider.class)
+	public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+		return new JwtAuthenticationProvider(userDetailsService,passwordEncoder);
 	}
 }
