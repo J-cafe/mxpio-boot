a088c0b7b77a35c95c4191de55a48dc5bfccaf20
MxpIO <i@mxpio.com>
2022-11-05 19:06:45 +0800

    update version

--- mxpio-boot-base-common/src/main/java/com/mxpioframework/common/swagger/SpringDocConfig.java
+++ mxpio-boot-base-common/src/main/java/com/mxpioframework/common/swagger/SpringDocConfig.java
@@ -19,7 +19,7 @@ public class SpringDocConfig {
 	@Value("${mxpio.swagger.desc:MxpIO-Boot API}")
 	private String swaggerDesc;
 	
-	@Value("${mxpio.swagger.version:1.0.12-beta.6}")
+	@Value("${mxpio.swagger.version:1.0.12-beta.7}")
 	private String swaggerVersion;
 	
 	@Value("${mxpio.swagger.author:MxpIO}")
