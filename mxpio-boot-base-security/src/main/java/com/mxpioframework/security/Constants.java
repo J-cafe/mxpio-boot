7611dd23b17f9284f18a0fcae6eb1b171061b6ac
hd-wupanpan <252437792@qq.com>
2022-11-16 19:47:06 +0800

    新增获取系统配置资源接口

--- mxpio-boot-base-security/src/main/java/com/mxpioframework/security/Constants.java
+++ mxpio-boot-base-security/src/main/java/com/mxpioframework/security/Constants.java
@@ -54,4 +54,34 @@ public class Constants {
 		}
 	}
 
+	public enum RegexEnum {//密码校验 正则表达式
+		DIGANDLETTER("/^(?=.*[0-9].*)(?=.*[A-Z].*)(?=.*[a-z].*)/", "包含数字、大小写字母"),
+		CONTINUOUS("/(?!.*(?<re1>[a-zA-Z0-9])\\k<re1>{3})(?<!.*(?<re2>[a-zA-Z0-9])\\k<re2>{3})/", "不能是连续的数字、字母"),
+		MINLENGTH("12", "最短长度");
+
+		RegexEnum(String code, String name) {
+			this.code = code;
+			this.name = name;
+		}
+
+		private String code;
+
+		private String name;
+
+		public String getCode() {
+			return code;
+		}
+
+		public void setCode(String code) {
+			this.code = code;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+	}
 }
--- mxpio-boot-base-security/src/main/java/com/mxpioframework/security/controller/SystemController.java
+++ mxpio-boot-base-security/src/main/java/com/mxpioframework/security/controller/SystemController.java
@@ -3,12 +3,13 @@ package com.mxpioframework.security.controller;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.Base64;
-import java.util.UUID;
+import java.util.*;
 
 import javax.imageio.ImageIO;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -39,7 +40,11 @@ public class SystemController {
 	
 	@Autowired
 	private OnlineUserService onlineUserService;
-	
+
+	@Value("${mxpio.captcha.open}")
+	private String captchaOpenFlag;
+	@Value("${mxpio.password.strategy}")
+	private String passwordStrategy;
 	@GetMapping("captcha")
 	@Operation(summary = "加载验证码", description = "获取登录验证码", method = "GET")
 	public Result<CaptchaDTO> captcha() throws IOException {
@@ -70,5 +75,43 @@ public class SystemController {
 		}else {
 			return Result.noauth403("refreshToken已失效");
 		}
+	}
+
+	@GetMapping("loadConfigResource")
+	@Operation(summary = "获取后端资源配置", description = "获取后端资源配置", method = "GET")
+	public Result<Map<String,Object>> loadConfigResource() {
+		Map<String,Object> returnMap = new HashMap<>();
+		if (StringUtils.equals("true",captchaOpenFlag)){
+			returnMap.put("captchaOpenFlag",Boolean.TRUE);
+		}else{
+			returnMap.put("captchaOpenFlag",Boolean.FALSE);
+		}
+
+		if (StringUtils.isNotBlank(passwordStrategy)){
+			String[] split = passwordStrategy.split(",");
+			List<Map<String,String>> configRegexs = new ArrayList<>();
+			for (String per:split){
+				Map<String,String> patternMap = new HashMap<>();
+				switch (per){
+					case "DIGANDLETTER":
+						patternMap.put("pattern",Constants.RegexEnum.DIGANDLETTER.getCode());
+						patternMap.put("message",Constants.RegexEnum.DIGANDLETTER.getName());
+						configRegexs.add(patternMap);
+						break;
+					case "CONTINUOUS":
+						patternMap.put("pattern",Constants.RegexEnum.CONTINUOUS.getCode());
+						patternMap.put("message",Constants.RegexEnum.CONTINUOUS.getName());
+						configRegexs.add(patternMap);
+						break;
+					case "MINLENGTH":
+						patternMap.put("min",Constants.RegexEnum.MINLENGTH.getCode());
+						patternMap.put("message",Constants.RegexEnum.MINLENGTH.getName());
+						configRegexs.add(patternMap);
+						break;
+				}
+			}
+			returnMap.put("passwordStrategy",configRegexs);
+		}
+		return Result.OK(returnMap);
 	}
 }
