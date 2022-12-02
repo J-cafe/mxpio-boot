7eed93713a626c307ef10f8ee4a26777e8ed180d
MxpIO <i@mxpio.com>
2022-12-02 16:08:33 +0800

    传参方式

--- mxpio-boot-base-system/src/main/java/com/mxpioframework/system/controller/CommonController.java
+++ mxpio-boot-base-system/src/main/java/com/mxpioframework/system/controller/CommonController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mxpioframework.common.vo.Result;
@@ -25,7 +26,7 @@ public class CommonController {
 	public Result<Long> duplicate(@PathVariable(name = "tableName", required = true) String tableName,
 			@PathVariable(name = "column", required = true) String column,
 			@PathVariable(name = "key", required = true) String key,
-			@PathVariable(name = "exclude", required = false) String exclude) {
+			@RequestParam(name = "exclude", required = false) String exclude) {
 		
 		Long count = commonService.duplicate(tableName, column, key,exclude);
 		return Result.OK(count);
