b7a2a1da6f0a41269f116d8b1f77160dbbc56099
MxpIO <i@mxpio.com>
2023-07-22 16:01:58 +0800

    字典配置

--- mxpio-boot-base-camunda/src/main/java/com/mxpioframework/camunda/vo/TaskVO.java
+++ mxpio-boot-base-camunda/src/main/java/com/mxpioframework/camunda/vo/TaskVO.java
@@ -2,11 +2,14 @@ package com.mxpioframework.camunda.vo;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.camunda.bpm.engine.history.HistoricProcessInstance;
 import org.camunda.bpm.engine.history.HistoricTaskInstance;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.mxpioframework.jpa.annotation.DictAble;
 import com.mxpioframework.security.annotation.Dict;
 import com.mxpioframework.security.entity.User;
 
@@ -14,47 +17,47 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
-public class TaskVO implements Serializable {
+public class TaskVO implements Serializable, DictAble {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Schema(description = "ID")
 	private String id;
-	
-	@Dict(dicCode="username", dicEntity = User.class, dicText = "nickname")
+
+	@Dict(dicCode = "username", dicEntity = User.class, dicText = "nickname")
 	@Schema(description = "当前审批人")
 	private String assignee;
-	
+
 	@Schema(description = "节点名称")
 	private String name;
-	
+
 	@Schema(description = "节点定义Key")
 	private String taskDefinitionKey;
-	
+
 	@Schema(description = "流程实例ID")
 	private String processInstanceId;
-	
+
 	@Schema(description = "流程定义ID")
 	private String processDefinitionId;
-	
+
 	@Schema(description = "流程定义Key")
 	private String processDefinitionKey;
-	
+
 	@Schema(description = "创建时间")
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
 	private Date createTime;
-	
+
 	@Schema(description = "流程发起时间")
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
 	private Date procStartTime;
-	
+
 	@Schema(description = "发起人")
-	@Dict(dicCode="username", dicEntity = User.class, dicText = "nickname")
+	@Dict(dicCode = "username", dicEntity = User.class, dicText = "nickname")
 	private String procStartUserId;
-	
+
 	@Schema(description = "流程定义名称")
 	private String processDefinitionName;
-	
+
 	public TaskVO(HistoricTaskInstance task) {
 		this.id = task.getId();
 		this.assignee = task.getAssignee();
@@ -70,6 +73,15 @@ public class TaskVO implements Serializable {
 		this.procStartTime = historicProcessInstance.getStartTime();
 		this.procStartUserId = historicProcessInstance.getStartUserId();
 		this.processDefinitionName = historicProcessInstance.getProcessDefinitionName();
+	}
+
+	private Map<String, String> textMap;
+
+	public String putText(String key, String value) {
+		if (textMap == null) {
+			textMap = new HashMap<>();
+		}
+		return textMap.put(key, value);
 	}
 
 }
