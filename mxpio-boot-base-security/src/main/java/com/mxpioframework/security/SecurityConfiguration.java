e45aac506403d66c0ca41cde54bdd6acbbec0b8b
MxpIO <i@mxpio.com>
2023-10-08 17:51:16 +0800

    fix

--- mxpio-boot-base-security/src/main/java/com/mxpioframework/security/SecurityConfiguration.java
+++ mxpio-boot-base-security/src/main/java/com/mxpioframework/security/SecurityConfiguration.java
@@ -1,19 +1,11 @@
 package com.mxpioframework.security;
 
-import java.util.List;
-
 import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import com.mxpioframework.security.access.policy.PasswordCheckPolicy;
-import com.mxpioframework.security.anthentication.JwtAuthenticationProvider;
 
 @Configuration
 @AutoConfigurationPackage
