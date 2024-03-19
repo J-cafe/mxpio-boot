2a790b2c776d2f93dc2410f7455628ea440545ed
wupp <wupp@datazhzh.com>
2024-03-19 14:35:06 +0800

    add:dbconsole增加sqlserver2022版本号支持

---
+++ mxpio-boot-base-dbconsole/src/main/java/com/mxpioframework/dbconsole/jdbc/dialect/SQLServer2022Dialect.java
@@ -0,0 +1,14 @@
+package com.mxpioframework.dbconsole.jdbc.dialect;
+
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+
+@Component
+public class SQLServer2022Dialect extends SQLServer2005Dialect {
+
+	@Override
+	public boolean support(Connection connection) {
+		return support(connection, "sql server", "16");
+	}
+}
