defa298650d8dac7fa3ad3255a2dd4d134db79f2
mxpio <i@mxpio.com>
2024-08-26 17:08:22 +0800

    fix Camunda 消息错误

--- mxpio-boot-base-camunda/src/main/java/com/mxpioframework/camunda/plugin/CamundaGlobalListenerDelegate.java
+++ mxpio-boot-base-camunda/src/main/java/com/mxpioframework/camunda/plugin/CamundaGlobalListenerDelegate.java
@@ -25,7 +25,7 @@ public class CamundaGlobalListenerDelegate implements ExecutionListener, TaskLis
 
     @Override
     public void notify(DelegateExecution delegateExecution) {
-        if("start".equals(delegateExecution.getEventName())){
+        if("end".equals(delegateExecution.getEventName())){
             MessageService messageService = SpringUtil.getBean(MessageService.class);
             Map<String, Object> props = delegateExecution.getVariables();
 
@@ -34,7 +34,7 @@ public class CamundaGlobalListenerDelegate implements ExecutionListener, TaskLis
             ProcessInstance processInstance = runtimeService.createProcessInstanceQuery()
                     .processInstanceId(delegateExecution.getProcessInstanceId())
                     .singleResult();
-            if(processInstance != null){
+            if(processInstance == null){
                 messageService.sendMessage(new String[]{"innerMsg"},"admin",new String[]{props.get(CamundaConstant.BPMN_START_USER).toString()},"流程完成通知:"+props.get(CamundaConstant.BPMN_TITLE),"流程【"+props.get(CamundaConstant.BPMN_TITLE)+"】已完成");
                 /*ActivityInstance[] instances = runtimeService.getActivityInstance(processInstance.getId()).getChildActivityInstances();
                 if(instances != null && instances.length == 1 && instances[0].getActivityType().endsWith("EndEvent")){
