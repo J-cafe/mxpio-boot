a01e03bc879b118aec874617c30d6d83c5a871c2
mxpio <i@mxpio.com>
2024-10-09 14:01:47 +0800

    多租户

--- mxpio-boot-base-system/src/main/java/com/mxpioframework/system/entity/SNExpression.java
+++ mxpio-boot-base-system/src/main/java/com/mxpioframework/system/entity/SNExpression.java
@@ -2,13 +2,6 @@ package com.mxpioframework.system.entity;
 
 import lombok.Data;
 
-/**
- * @Author : wpp
- * @Datetime : 2023/8/11 18:21
- * @Desc :
- * @Modor :  Modifytime:
- * @modDesc :
- */
 @Data
 public class SNExpression {
     /*
