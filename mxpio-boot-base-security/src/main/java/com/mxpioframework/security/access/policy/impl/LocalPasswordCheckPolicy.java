199924c891abe9eb53deeaefe4871988b75536e4
wupaa <wupp@zz.com>
2025-09-02 11:33:04 +0800

    feat:登录增加传输密码解密

--- mxpio-boot-base-security/src/main/java/com/mxpioframework/security/access/policy/impl/LocalPasswordCheckPolicy.java
+++ mxpio-boot-base-security/src/main/java/com/mxpioframework/security/access/policy/impl/LocalPasswordCheckPolicy.java
@@ -1,5 +1,8 @@
 package com.mxpioframework.security.access.policy.impl;
 
+import com.mxpioframework.security.util.AesEncryptUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -9,6 +12,8 @@ import com.mxpioframework.security.anthentication.JwtLoginToken;
 
 @Component
 public class LocalPasswordCheckPolicy implements PasswordCheckPolicy {
+	@Value("${mxpio.JSEncrypt.privateKey:}")
+	private String privateKey;
 	
 	@Override
 	public boolean support(String passwordCheckPolicyName) {
@@ -21,6 +26,13 @@ public class LocalPasswordCheckPolicy implements PasswordCheckPolicy {
 			return false;
 		}
 		String presentedPassword = authentication.getCredentials().toString();
+		if (StringUtils.isNotBlank(privateKey)&&!StringUtils.equals("null",privateKey)) {
+            try {
+                presentedPassword = AesEncryptUtil.decrypt(presentedPassword, privateKey);
+            } catch (Exception e) {
+                throw new RuntimeException(e+"密码解密失败");
+            }
+        }
 		if (!passwordEncoder.matches(presentedPassword, userDetails.getPassword())) {
 			return false;
 		}
